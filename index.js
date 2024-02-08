e496b05d8f17223eb970b69329a1759d2bdbc98f
VisaganR <visagan126@gmail.com>
2024-02-08 12:45:32 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require("express")
+const mongoose= require("mongoose");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+const app = express ();
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+
+mongoose.connect("mongodb+srv://visagan126:visa1206@cluster0.xvaxybi.mongodb.net/registrationFormDB",{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    });
+    // registration schema
+    const registrationSchema= new mongoose. Schema ({
+    name: String,
+    email: String,
+    password: String
+    });
+    // model of registration schema
+    const Registration = mongoose.model("Registration", registrationSchema);
+    
+    app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.json());
+
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/pages/index.html");
+    })
+
+    app.post("/register",async (req, res) => {
+        try{
+        const {name, email, password} = req.body;
+        const existingUser=await Registration.findOne ({email: email});
+if(!existingUser) {
+    const registrationData =new Registration({
+        name,
+        email,
+        password
+    });
+   await registrationData.save()
+   res.redirect("/Success");
+}
+else{
+    console.log("USer already exist")
+    res.redirect("/error");
+}
+       
+    }catch(error){
+        console.log(error)
+    res.redirect("error")
+    }
+        })
+        app.get("/success",(req,res)=>{
+            res.sendFile(__dirname+"/pages/success.html");
+        })
+        app.get("/error",(req,res)=>{
+            res.sendFile(__dirname+"/pages/error.html");
+        })
+
+
+app.listen(port, ()=>{
+console.log(`server is running on port ${port}`);
+})
